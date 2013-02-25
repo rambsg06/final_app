b8f0920a10d49be60f1a18fa9af2707abf8afaee
Rameswar Rath <ram@gmicro.us>
2013-02-25 14:22:27 +0530

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def full_title(page_title)
+    full_title = "Ruby on Rails Tutorial App"
+    page_title.present? ? (full_title += " | #{page_title}") : full_title
+  end
 end
